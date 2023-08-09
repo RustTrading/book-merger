7bbaad43120f2dfcd43aab50b3d834fb2bcae49b
maximsamsonov <maxim.samsonov@aditazz.com>
2023-08-09 19:49:06 +0300

    aggregation

--- src/exchange_tools.rs
+++ src/exchange_tools.rs
@@ -1,15 +1,57 @@
 use serde::{Serialize, Deserialize};
 use tokio_tungstenite::tungstenite::protocol::Message;
 use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
+use std::collections::BTreeMap;
 
 pub const BITSTAMP_WSS: &str = "wss://ws.bitstamp.net";
 pub const BINANCE_WSS_ETHBTC_20: &str = "wss://stream.binance.com:9443/ws/ethbtc@depth20@100ms";
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct OrderBook {
+  pub bids: Vec<(Decimal, Decimal)>,
+  pub asks: Vec<(Decimal, Decimal)>,
+}
 
 #[derive(Debug, Clone, Copy)]
 pub enum Exchanges {
   Bitstamp(&'static str),
   Binance(&'static str),
 }
+
+pub struct AggregatedBook {
+  asks: BTreeMap<Decimal, Decimal>,
+  bids: BTreeMap<Decimal, Decimal>,
+  spread: Decimal,
+}
+
+impl AggregatedBook {
+  pub fn new() -> Self {
+    Self {
+      asks: BTreeMap::new(),
+      bids: BTreeMap::new(),
+      spread: dec!(0),
+    }
+  } 
+  pub fn update(&mut self, orderbook: OrderBook) {
+     for (price, volume) in orderbook.asks.into_iter() {
+      let mut aggregated = volume;  
+      if let Some(value) = self.asks.get(&price) {
+          aggregated += value;
+        } 
+        self.asks.insert(price, aggregated);
+     }
+     for (price, volume) in orderbook.bids.into_iter() {
+      let mut aggregated = volume;  
+      if let Some(value) = self.asks.get(&price) {
+          aggregated += value;
+        } 
+        self.bids.insert(price, aggregated);
+     }
+     self.spread = self.asks.first_key_value().unwrap().0 - self.bids.last_key_value().unwrap().0;
+  }
+}
+
 
 impl Exchanges {
   pub fn value(self) -> &'static str {
@@ -18,12 +60,6 @@ impl Exchanges {
       Self::Binance(value) => value,
     }
   }
-}
-
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct OrderBook {
-  pub bids: Vec<(Decimal, Decimal)>,
-  pub asks: Vec<(Decimal, Decimal)>,
 }
 
 use crate::{binance, bitstamp};
